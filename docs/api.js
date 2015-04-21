e89450233f82e2c593265128cf2049575660c0c5
Marcel Erz <me@marcelerz.com>
2015-04-20 22:52:50 -0700

    Update API-documentation and Coverage report

--- docs/api.js
+++ docs/api.js
@@ -1,9 +1,61 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
-        "PNGImage"
+        "CRC",
+        "Chunk",
+        "Compressor",
+        "Decoder",
+        "Encoder",
+        "Filter",
+        "IDAT",
+        "IEND",
+        "IHDR",
+        "Interlace",
+        "PLTE",
+        "PNGImage",
+        "Scale",
+        "bKGD",
+        "cHRM",
+        "chunkUtils",
+        "constants",
+        "gAMA",
+        "hIST",
+        "iCCP",
+        "iTXt",
+        "pHYs",
+        "sBIT",
+        "sPLT",
+        "sRGB",
+        "tEXt",
+        "tIME",
+        "tRNS",
+        "utils",
+        "zTXt"
     ],
-    "modules": [],
-    "allModules": []
+    "modules": [
+        "Core",
+        "PNG",
+        "PNGChunks",
+        "PNGCore"
+    ],
+    "allModules": [
+        {
+            "displayName": "Core",
+            "name": "Core",
+            "description": "PNGjs-image class"
+        },
+        {
+            "displayName": "PNG",
+            "name": "PNG"
+        },
+        {
+            "displayName": "PNGChunks",
+            "name": "PNGChunks"
+        },
+        {
+            "displayName": "PNGCore",
+            "name": "PNGCore"
+        }
+    ]
 } };
 });
