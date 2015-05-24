9ea2d2a473bb0bb84db501b09064e984eb41d398
Marcel Erz <me@marcelerz.com>
2015-05-24 03:53:40 -0700

    Add background color chunk tests with fix for value scaling

--- test/png/decode/ancillary/background.js
+++ test/png/decode/ancillary/background.js
@@ -1,14 +1,242 @@
 // Copyright 2015 Yahoo! Inc.
 // Copyrights licensed under the Mit License. See the accompanying LICENSE file for terms.
 
-//bgai4a08 - 8 bit grayscale, alpha, no background chunk, interlaced
-//bgai4a16 - 16 bit grayscale, alpha, no background chunk, interlaced
-//bgan6a08 - 3x8 bits rgb color, alpha, no background chunk
-//bgan6a16 - 3x16 bits rgb color, alpha, no background chunk
-//bgbn4a08 - 8 bit grayscale, alpha, black background chunk
-//bggn4a16 - 16 bit grayscale, alpha, gray background chunk
-//bgwn6a08 - 3x8 bits rgb color, alpha, white background chunk
-//bgyn6a16 - 3x16 bits rgb color, alpha, yellow background chunk
+var testGen = require('../../testGen');
+var expect = require('chai').expect;
+
 describe('Background', function () {
 
+	describe('No Background', function () {
+
+		describe('GrayScale with Alpha - Interlaced', function () {
+
+			describe('8-bit', function () {
+
+				testGen.addTests({
+					resourceGroup: ['ancillary', 'background'],
+					resourceFile: 'bgai4a08',
+					imageCheck: true,
+
+					chunkTypes: ['gAMA'],
+
+					headerCheck: true,
+					width: 32,
+					height: 32,
+					bitDepth: 8,
+					colorType: 4,
+					filter: 0,
+					compression: 0,
+					interlace: 1
+				});
+
+				it('should have no background color', function () {
+					expect(this.data.backgroundColor).to.be.undefined;
+				});
+			});
+
+			describe('16-bit', function () {
+
+				testGen.addTests({
+					resourceGroup: ['ancillary', 'background'],
+					resourceFile: 'bgai4a16',
+					imageCheck: true,
+
+					chunkTypes: ['gAMA'],
+
+					headerCheck: true,
+					width: 32,
+					height: 32,
+					bitDepth: 16,
+					colorType: 4,
+					filter: 0,
+					compression: 0,
+					interlace: 1
+				});
+
+				it('should have no background color', function () {
+					expect(this.data.backgroundColor).to.be.undefined;
+				});
+			});
+		});
+
+		describe('True-Color with Alpha - Non-Interlaced', function () {
+
+			describe('8-bit', function () {
+
+				testGen.addTests({
+					resourceGroup: ['ancillary', 'background'],
+					resourceFile: 'bgan6a08',
+					imageCheck: true,
+
+					chunkTypes: ['gAMA'],
+
+					headerCheck: true,
+					width: 32,
+					height: 32,
+					bitDepth: 8,
+					colorType: 6,
+					filter: 0,
+					compression: 0,
+					interlace: 0
+				});
+
+				it('should have no background color', function () {
+					expect(this.data.backgroundColor).to.be.undefined;
+				});
+			});
+
+			describe('16-bit', function () {
+
+				testGen.addTests({
+					resourceGroup: ['ancillary', 'background'],
+					resourceFile: 'bgan6a16',
+					imageCheck: true,
+
+					chunkTypes: ['gAMA'],
+
+					headerCheck: true,
+					width: 32,
+					height: 32,
+					bitDepth: 16,
+					colorType: 6,
+					filter: 0,
+					compression: 0,
+					interlace: 0
+				});
+
+				it('should have no background color', function () {
+					expect(this.data.backgroundColor).to.be.undefined;
+				});
+			});
+		});
+	});
+
+	describe('With Background', function () {
+
+		describe('GrayScale with Alpha', function () {
+
+			describe('8-bit', function () {
+
+				testGen.addTests({
+					resourceGroup: ['ancillary', 'background'],
+					resourceFile: 'bgbn4a08', // black background
+					imageCheck: true,
+
+					chunkTypes: ['gAMA'],
+
+					headerCheck: true,
+					width: 32,
+					height: 32,
+					bitDepth: 8,
+					colorType: 4,
+					filter: 0,
+					compression: 0,
+					interlace: 0
+				});
+
+				it('should have no background color', function () {
+					expect(this.data.backgroundColor).to.be.not.undefined;
+				});
+
+				it('should have black as a background color', function () {
+					expect(this.data.backgroundColor.red).to.be.equal(0);
+					expect(this.data.backgroundColor.green).to.be.equal(0);
+					expect(this.data.backgroundColor.blue).to.be.equal(0);
+				});
+			});
+
+			describe('16-bit', function () {
+
+				testGen.addTests({
+					resourceGroup: ['ancillary', 'background'],
+					resourceFile: 'bggn4a16', // gray background
+					imageCheck: true,
+
+					chunkTypes: ['gAMA'],
+
+					headerCheck: true,
+					width: 32,
+					height: 32,
+					bitDepth: 16,
+					colorType: 4,
+					filter: 0,
+					compression: 0,
+					interlace: 0
+				});
+
+				it('should have no background color', function () {
+					expect(this.data.backgroundColor).to.be.not.undefined;
+				});
+
+				it('should have black as a background color', function () {
+					expect(this.data.backgroundColor.red).to.be.equal(170);
+					expect(this.data.backgroundColor.green).to.be.equal(170);
+					expect(this.data.backgroundColor.blue).to.be.equal(170);
+				});
+			});
+		});
+
+		describe('True-Color with Alpha', function () {
+
+			describe('8-bit', function () {
+
+				testGen.addTests({
+					resourceGroup: ['ancillary', 'background'],
+					resourceFile: 'bgwn6a08', // white background
+					imageCheck: true,
+
+					chunkTypes: ['gAMA'],
+
+					headerCheck: true,
+					width: 32,
+					height: 32,
+					bitDepth: 8,
+					colorType: 6,
+					filter: 0,
+					compression: 0,
+					interlace: 0
+				});
+
+				it('should have no background color', function () {
+					expect(this.data.backgroundColor).to.be.not.undefined;
+				});
+
+				it('should have black as a background color', function () {
+					expect(this.data.backgroundColor.red).to.be.equal(255);
+					expect(this.data.backgroundColor.green).to.be.equal(255);
+					expect(this.data.backgroundColor.blue).to.be.equal(255);
+				});
+			});
+
+			describe('16-bit', function () {
+
+				testGen.addTests({
+					resourceGroup: ['ancillary', 'background'],
+					resourceFile: 'bgyn6a16', // yellow background
+					imageCheck: true,
+
+					chunkTypes: ['gAMA'],
+
+					headerCheck: true,
+					width: 32,
+					height: 32,
+					bitDepth: 16,
+					colorType: 6,
+					filter: 0,
+					compression: 0,
+					interlace: 0
+				});
+
+				it('should have no background color', function () {
+					expect(this.data.backgroundColor).to.be.not.undefined;
+				});
+
+				it('should have black as a background color', function () {
+					expect(this.data.backgroundColor.red).to.be.equal(255);
+					expect(this.data.backgroundColor.green).to.be.equal(255);
+					expect(this.data.backgroundColor.blue).to.be.equal(0);
+				});
+			});
+		});
+	});
 });
