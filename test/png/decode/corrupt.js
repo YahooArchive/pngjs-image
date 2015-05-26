5930dd2d203e74beacdbc2f4ec8546c977346fa7
Marcel Erz <me@marcelerz.com>
2015-05-26 01:22:45 -0700

    Add corrupt tests

--- test/png/decode/corrupt.js
+++ test/png/decode/corrupt.js
@@ -1,22 +1,132 @@
 // Copyright 2015 Yahoo! Inc.
 // Copyrights licensed under the Mit License. See the accompanying LICENSE file for terms.
 
-//xs1n0g01 - signature byte 1 MSBit reset to zero
-//xs2n0g01 - signature byte 2 is a 'Q'
-//xs4n0g01 - signature byte 4 lowercase
-//xs7n0g01 - 7th byte a space instead of control-Z
-//xcrn0g04 - added cr bytes
-//xlfn0g04 - added lf bytes
 //xhdn0g08 - incorrect IHDR checksum
-//xc1n0g08 - color type 1
-//xc9n2c08 - color type 9
-//xd0n2c08 - bit-depth 0
-//xd3n2c08 - bit-depth 3
-//xd9n2c08 - bit-depth 99
-//xdtn0g01 - missing IDAT chunk
 //xcsn0g01 - incorrect IDAT checksum
 var testGen = require('../testGen');
 
 describe('Corrupted files', function () {
 
+	describe('Signature', function () {
+
+		describe('Signature byte 1 MSBit reset to zero', function () {
+			testGen.addTests({
+				resourceGroup: 'corrupt',
+				resourceFile: 'xs2n0g01',
+				expectFailure: true,
+				expectMessage: 'Invalid signature for a PNG image.'
+			});
+		});
+
+		describe('Signature byte 2 is a "Q"', function () {
+			testGen.addTests({
+				resourceGroup: 'corrupt',
+				resourceFile: 'xs1n0g01',
+				expectFailure: true,
+				expectMessage: 'Invalid signature for a PNG image.'
+			});
+		});
+
+		describe('Signature byte 4 lowercase', function () {
+			testGen.addTests({
+				resourceGroup: 'corrupt',
+				resourceFile: 'xs4n0g01',
+				expectFailure: true,
+				expectMessage: 'Invalid signature for a PNG image.'
+			});
+		});
+
+		describe('7th byte a space instead of control-Z', function () {
+			testGen.addTests({
+				resourceGroup: 'corrupt',
+				resourceFile: 'xs7n0g01',
+				expectFailure: true,
+				expectMessage: 'Invalid signature for a PNG image.'
+			});
+		});
+
+		describe('added cr bytes', function () {
+			testGen.addTests({
+				resourceGroup: 'corrupt',
+				resourceFile: 'xcrn0g04',
+				expectFailure: true,
+				expectMessage: 'Invalid signature for a PNG image.'
+			});
+		});
+
+		describe('added lf bytes', function () {
+			testGen.addTests({
+				resourceGroup: 'corrupt',
+				resourceFile: 'xlfn0g04',
+				expectFailure: true,
+				expectMessage: 'Invalid signature for a PNG image.'
+			});
+		});
+	});
+
+	describe('Header', function () {
+
+		describe('Color-Type', function () {
+
+			describe('1', function () {
+				testGen.addTests({
+					resourceGroup: 'corrupt',
+					resourceFile: 'xc1n0g08',
+					expectFailure: true,
+					expectMessage: 'Unknown color-type 1.'
+				});
+			});
+
+			describe('9', function () {
+				testGen.addTests({
+					resourceGroup: 'corrupt',
+					resourceFile: 'xc9n2c08',
+					expectFailure: true,
+					expectMessage: 'Unknown color-type 9.'
+				});
+			});
+		});
+
+		describe('Bit-Depth', function () {
+
+			describe('0', function () {
+				testGen.addTests({
+					resourceGroup: 'corrupt',
+					resourceFile: 'xd0n2c08',
+					expectFailure: true,
+					expectMessage: 'Unknown bit-depth of 0.'
+				});
+			});
+
+			describe('3', function () {
+				testGen.addTests({
+					resourceGroup: 'corrupt',
+					resourceFile: 'xd3n2c08',
+					expectFailure: true,
+					expectMessage: 'Unknown bit-depth of 3.'
+				});
+			});
+
+			describe('99', function () {
+				testGen.addTests({
+					resourceGroup: 'corrupt',
+					resourceFile: 'xd9n2c08',
+					expectFailure: true,
+					expectMessage: 'Unknown bit-depth of 99.'
+				});
+			});
+		});
+	});
+
+	describe('Data', function () {
+
+		describe('Missing IDAT', function () {
+			testGen.addTests({
+				resourceGroup: 'corrupt',
+				resourceFile: 'xdtn0g01',
+				expectFailure: true,
+				expectMessage: 'Could not retrieve chunk type IDAT.'
+			});
+		});
+	});
 });
--- test/png/testGen.js
+++ test/png/testGen.js
@@ -10,6 +10,8 @@ module.exports = {
 	 * @param {object} options
 	 * @param {string} options.resourceGroup
 	 * @param {string} options.resourceFile
+	 * @param {boolean} [options.expectFailure]
+	 * @param {string} [options.expectMessage]
 	 * @param {object} [options.decodeOptions]
 	 * @param {string[]} [options.chunkTypes]
 	 * @param {boolean} [options.imageCheck=false]
@@ -29,90 +31,102 @@ module.exports = {
 		});
 
 		it('should decode', function () {
-			this.decode(this.file, options.decodeOptions);
-		});
-
-		it('should have mandatory chunks', function () {
-			expect(this.chunks).to.contain.keys('IDAT', 'IHDR', 'IEND');
-		});
-
-		if (options.chunkTypes) {
-			it('should have chunks', function () {
-				expect(this.chunks).to.contain.keys(options.chunkTypes);
-			});
-		}
-
-		if (options.imageCheck) {
-			it('should load the image buffer', function () {
-
-				var pathRaw = this.resource(options.resourceGroup, options.resourceFile + '.raw'),
-					pathPng = this.resource(options.resourceGroup, options.resourceFile + '_trueColor.png');
-
-				if (!fs.existsSync(pathRaw)) {
-
-					fs.writeFileSync(pathRaw, this.image);
-					this.encode(pathPng, this.image, options.width, options.height);
-
-					throw new Error('Compare-to image did not exist. Please check manually the image and check it in.');
+			try {
+				this.decode(this.file, options.decodeOptions);
+			} catch (err) {
+				if (options.expectFailure) {
+					if (options.expectMessage != err.message) {
+						throw new Error('Error message was not expected: ' + err.message);
+					}
+				} else {
+					throw err;
 				}
-
-				this.compareToFile(this.image, pathRaw);
-			});
-		}
-
-		describe('Data', function () {
-
-			it('should have volatile data', function () {
-				if (options.headerCheck) {
-					expect(this.data).to.contain.key('volatile');
-				}
-			});
-
-			if (options.headerCheck) {
-
-				describe('Header', function () {
-
-					before(function () {
-						expect(this.data.volatile.header).to.be.not.undefined;
-						this.header = this.data.volatile.header;
-					});
-
-					it('should have the right resolution', function () {
-						expect(this.header.width).to.be.equal(options.width || 32);
-						expect(this.header.height).to.be.equal(options.height || 32);
-					});
-
-					if (options.bitDepth !== undefined) {
-						it('should have the correct bit-depth', function () {
-							expect(this.header.bitDepth).to.be.equal(options.bitDepth);
-						});
-					}
-
-					if (options.colorType !== undefined) {
-						it('should have the correct color-type', function () {
-							expect(this.header.colorType).to.be.equal(options.colorType);
-						});
-					}
-
-					if (options.filter !== undefined) {
-						it('should have the correct filter method', function () {
-							expect(this.header.filter).to.be.equal(options.filter);
-						});
-					}
-
-					if (options.compression !== undefined) {
-						it('should have the correct compression method', function () {
-							expect(this.header.compression).to.be.equal(options.compression);
-						});
-					}
-
-					if (options.interlace !== undefined) {
-						it('should have the correct interlace method', function () {
-							expect(this.header.interlace).to.be.equal(options.interlace);
-						});
-					}
-				});
 			}
 		});
+
+		if (!options.expectFailure) {
+
+			it('should have mandatory chunks', function () {
+				expect(this.chunks).to.contain.keys('IDAT', 'IHDR', 'IEND');
+			});
+
+			if (options.chunkTypes) {
+				it('should have chunks', function () {
+					expect(this.chunks).to.contain.keys(options.chunkTypes);
+				});
+			}
+
+			if (options.imageCheck) {
+				it('should load the image buffer', function () {
+
+					var pathRaw = this.resource(options.resourceGroup, options.resourceFile + '.raw'), pathPng = this.resource(options.resourceGroup, options.resourceFile + '_trueColor.png');
+
+					if (!fs.existsSync(pathRaw)) {
+
+						fs.writeFileSync(pathRaw, this.image);
+						this.encode(pathPng, this.image, options.width, options.height);
+
+						throw new Error('Compare-to image did not exist. Please check manually the image and check it in.');
+					}
+
+					this.compareToFile(this.image, pathRaw);
+				});
+			}
+
+			describe('Data', function () {
+
+				it('should have volatile data', function () {
+					if (options.headerCheck) {
+						expect(this.data).to.contain.key('volatile');
+					}
+				});
+
+				if (options.headerCheck) {
+
+					describe('Header', function () {
+
+						before(function () {
+							expect(this.data.volatile.header).to.be.not.undefined;
+							this.header = this.data.volatile.header;
+						});
+
+						it('should have the right resolution', function () {
+							expect(this.header.width).to.be.equal(options.width || 32);
+							expect(this.header.height).to.be.equal(options.height || 32);
+						});
+
+						if (options.bitDepth !== undefined) {
+							it('should have the correct bit-depth', function () {
+								expect(this.header.bitDepth).to.be.equal(options.bitDepth);
+							});
+						}
+
+						if (options.colorType !== undefined) {
+							it('should have the correct color-type', function () {
+								expect(this.header.colorType).to.be.equal(options.colorType);
+							});
+						}
+
+						if (options.filter !== undefined) {
+							it('should have the correct filter method', function () {
+								expect(this.header.filter).to.be.equal(options.filter);
+							});
+						}
+
+						if (options.compression !== undefined) {
+							it('should have the correct compression method', function () {
+								expect(this.header.compression).to.be.equal(options.compression);
+							});
+						}
+
+						if (options.interlace !== undefined) {
+							it('should have the correct interlace method', function () {
+								expect(this.header.interlace).to.be.equal(options.interlace);
+							});
+						}
+					});
+				}
+			});
+		}
 	}
 };
