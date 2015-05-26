0269a93de6319f470a187bc9c310ea9b985c1586
Marcel Erz <me@marcelerz.com>
2015-05-26 01:01:24 -0700

    Add tests for transparency options

--- test/png/decode/ancillary/transparency.js
+++ test/png/decode/ancillary/transparency.js
@@ -4,123 +4,305 @@
 var testGen = require('../../testGen');
 var expect = require('chai').expect;
 
-//tbbn2c16 - transparent, blue background chunk
-//tbbn3p08 - transparent, black background chunk
-//tbgn2c16 - transparent, green background chunk
-//tbgn3p08 - transparent, light-gray background chunk
-//tbrn2c08 - transparent, red background chunk
-//tbwn3p08 - transparent, white background chunk
-//tbyn3p08 - transparent, yellow background chunk
-//tp1n3p08 - transparent, but no background chunk
-//tm3n3p02 - multiple levels of transparency, 3 entries
 describe('Transparency', function () {
 
-	//describe('Not Transparent', function () {
-  //
-	//	describe('GrayScale', function () {
-  //
-	//		testGen.addTests({
-	//			resourceGroup: ['ancillary', 'transparency'],
-	//			resourceFile: 'tp0n0g08',
-	//			imageCheck: true,
-  //
-	//			chunkTypes: ['gAMA'],
-  //
-	//			headerCheck: true,
-	//			width: 32,
-	//			height: 32,
-	//			bitDepth: 8,
-	//			colorType: 0,
-	//			filter: 0,
-	//			compression: 0,
-	//			interlace: 0
-	//		});
-	//	});
-  //
-	//	describe('True-Color', function () {
-  //
-	//		testGen.addTests({
-	//			resourceGroup: ['ancillary', 'transparency'],
-	//			resourceFile: 'tp0n2c08',
-	//			imageCheck: true,
-  //
-	//			chunkTypes: ['gAMA'],
-  //
-	//			headerCheck: true,
-	//			width: 32,
-	//			height: 32,
-	//			bitDepth: 8,
-	//			colorType: 2,
-	//			filter: 0,
-	//			compression: 0,
-	//			interlace: 0
-	//		});
-	//	});
-  //
-	//	describe('Indexed-Color', function () {
-  //
-	//		testGen.addTests({
-	//			resourceGroup: ['ancillary', 'transparency'],
-	//			resourceFile: 'tp0n3p08',
-	//			imageCheck: true,
-  //
-	//			chunkTypes: ['gAMA'],
-  //
-	//			headerCheck: true,
-	//			width: 32,
-	//			height: 32,
-	//			bitDepth: 8,
-	//			colorType: 3,
-	//			filter: 0,
-	//			compression: 0,
-	//			interlace: 0
-	//		});
-	//	});
-	//});
-  //
-	//describe('Transparent', function () {
-  //
-	//	describe('GrayScale', function () {
-  //
-	//		describe('4-bit', function () {
-  //
-	//			testGen.addTests({
-	//				resourceGroup: ['ancillary', 'transparency'],
-	//				resourceFile: 'tbbn0g04',
-	//				imageCheck: true,
-  //
-	//				chunkTypes: ['gAMA', 'tRNS', 'bKGD'],
-  //
-	//				headerCheck: true,
-	//				width: 32,
-	//				height: 32,
-	//				bitDepth: 4,
-	//				colorType: 0,
-	//				filter: 0,
-	//				compression: 0,
-	//				interlace: 0
-	//			});
-	//		});
-  //
-	//		describe('16-bit', function () {
-  //
-	//			testGen.addTests({
-	//				resourceGroup: ['ancillary', 'transparency'],
-	//				resourceFile: 'tbwn0g16',
-	//				imageCheck: true,
-  //
-	//				chunkTypes: ['gAMA', 'tRNS', 'bKGD'],
-  //
-	//				headerCheck: true,
-	//				width: 32,
-	//				height: 32,
-	//				bitDepth: 16,
-	//				colorType: 0,
-	//				filter: 0,
-	//				compression: 0,
-	//				interlace: 0
-	//			});
-	//		});
-	//	});
-	//});
+	describe('Not Transparent', function () {
+
+		describe('GrayScale', function () {
+
+			testGen.addTests({
+				resourceGroup: ['ancillary', 'transparency'],
+				resourceFile: 'tp0n0g08',
+				imageCheck: true,
+
+				chunkTypes: ['gAMA'],
+
+				headerCheck: true,
+				width: 32,
+				height: 32,
+				bitDepth: 8,
+				colorType: 0,
+				filter: 0,
+				compression: 0,
+				interlace: 0
+			});
+		});
+
+		describe('True-Color', function () {
+
+			testGen.addTests({
+				resourceGroup: ['ancillary', 'transparency'],
+				resourceFile: 'tp0n2c08',
+				imageCheck: true,
+
+				chunkTypes: ['gAMA'],
+
+				headerCheck: true,
+				width: 32,
+				height: 32,
+				bitDepth: 8,
+				colorType: 2,
+				filter: 0,
+				compression: 0,
+				interlace: 0
+			});
+		});
+
+		describe('Indexed-Color', function () {
+
+			testGen.addTests({
+				resourceGroup: ['ancillary', 'transparency'],
+				resourceFile: 'tp0n3p08',
+				imageCheck: true,
+
+				chunkTypes: ['gAMA'],
+
+				headerCheck: true,
+				width: 32,
+				height: 32,
+				bitDepth: 8,
+				colorType: 3,
+				filter: 0,
+				compression: 0,
+				interlace: 0
+			});
+		});
+	});
+
+	describe('Transparent', function () {
+
+		describe('GrayScale', function () {
+
+			describe('4-bit', function () {
+
+				testGen.addTests({
+					resourceGroup: ['ancillary', 'transparency'],
+					resourceFile: 'tbbn0g04',
+					imageCheck: true,
+
+					chunkTypes: ['gAMA', 'tRNS', 'bKGD'],
+
+					headerCheck: true,
+					width: 32,
+					height: 32,
+					bitDepth: 4,
+					colorType: 0,
+					filter: 0,
+					compression: 0,
+					interlace: 0
+				});
+			});
+
+			describe('16-bit', function () {
+
+				testGen.addTests({
+					resourceGroup: ['ancillary', 'transparency'],
+					resourceFile: 'tbwn0g16',
+					imageCheck: true,
+
+					chunkTypes: ['gAMA', 'tRNS', 'bKGD'],
+
+					headerCheck: true,
+					width: 32,
+					height: 32,
+					bitDepth: 16,
+					colorType: 0,
+					filter: 0,
+					compression: 0,
+					interlace: 0
+				});
+			});
+		});
+
+		describe('True-Color', function () {
+
+			describe('8-bit', function () {
+
+				testGen.addTests({
+					resourceGroup: ['ancillary', 'transparency'],
+					resourceFile: 'tbrn2c08',
+					imageCheck: true,
+
+					chunkTypes: ['gAMA', 'tRNS', 'bKGD'],
+
+					headerCheck: true,
+					width: 32,
+					height: 32,
+					bitDepth: 8,
+					colorType: 2,
+					filter: 0,
+					compression: 0,
+					interlace: 0
+				});
+			});
+
+			describe('16-bit - Use no background', function () {
+
+				testGen.addTests({
+					resourceGroup: ['ancillary', 'transparency'],
+					resourceFile: 'tbbn2c16',
+					imageCheck: true,
+
+					chunkTypes: ['gAMA', 'tRNS', 'bKGD'],
+
+					headerCheck: true,
+					width: 32,
+					height: 32,
+					bitDepth: 16,
+					colorType: 2,
+					filter: 0,
+					compression: 0,
+					interlace: 0
+				});
+			});
+
+			describe('16-bit - Use background', function () {
+
+				testGen.addTests({
+					resourceGroup: ['ancillary', 'transparency'],
+					resourceFile: 'tbgn2c16',
+					imageCheck: true,
+					decodeOptions: { background: true},
+
+					chunkTypes: ['gAMA', 'tRNS', 'bKGD'],
+
+					headerCheck: true,
+					width: 32,
+					height: 32,
+					bitDepth: 16,
+					colorType: 2,
+					filter: 0,
+					compression: 0,
+					interlace: 0
+				});
+			});
+		});
+
+		describe('Indexed-Color', function () {
+
+			describe('8-bit - Use no background', function () {
+
+				testGen.addTests({
+					resourceGroup: ['ancillary', 'transparency'],
+					resourceFile: 'tbbn3p08',
+					imageCheck: true,
+
+					chunkTypes: ['gAMA', 'tRNS', 'bKGD'],
+
+					headerCheck: true,
+					width: 32,
+					height: 32,
+					bitDepth: 8,
+					colorType: 3,
+					filter: 0,
+					compression: 0,
+					interlace: 0
+				});
+			});
+
+			describe('8-bit - Use background - Gray', function () {
+
+				testGen.addTests({
+					resourceGroup: ['ancillary', 'transparency'],
+					resourceFile: 'tbgn3p08',
+					imageCheck: true,
+					decodeOptions: { background: true, transparent: true },
+
+					chunkTypes: ['gAMA', 'tRNS', 'bKGD'],
+
+					headerCheck: true,
+					width: 32,
+					height: 32,
+					bitDepth: 8,
+					colorType: 3,
+					filter: 0,
+					compression: 0,
+					interlace: 0
+				});
+			});
+
+			describe('8-bit - Use background - White', function () {
+
+				testGen.addTests({
+					resourceGroup: ['ancillary', 'transparency'],
+					resourceFile: 'tbwn3p08',
+					imageCheck: true,
+					decodeOptions: { background: true },
+
+					chunkTypes: ['gAMA', 'tRNS', 'bKGD'],
+
+					headerCheck: true,
+					width: 32,
+					height: 32,
+					bitDepth: 8,
+					colorType: 3,
+					filter: 0,
+					compression: 0,
+					interlace: 0
+				});
+			});
+
+			describe('8-bit - Use background - No Transparency', function () {
+
+				testGen.addTests({
+					resourceGroup: ['ancillary', 'transparency'],
+					resourceFile: 'tbyn3p08',
+					imageCheck: true,
+					decodeOptions: { background: true, transparent: false },
+
+					chunkTypes: ['gAMA', 'tRNS', 'bKGD'],
+
+					headerCheck: true,
+					width: 32,
+					height: 32,
+					bitDepth: 8,
+					colorType: 3,
+					filter: 0,
+					compression: 0,
+					interlace: 0
+				});
+			});
+
+			describe('8-bit - No background', function () {
+
+				testGen.addTests({
+					resourceGroup: ['ancillary', 'transparency'],
+					resourceFile: 'tp1n3p08',
+					imageCheck: true,
+
+					chunkTypes: ['gAMA', 'tRNS'],
+
+					headerCheck: true,
+					width: 32,
+					height: 32,
+					bitDepth: 8,
+					colorType: 3,
+					filter: 0,
+					compression: 0,
+					interlace: 0
+				});
+			});
+		});
+	});
+
+	describe('Multi-Level Transparency', function () {
+
+		describe('2-bit', function () {
+
+			testGen.addTests({
+				resourceGroup: ['ancillary', 'transparency'],
+				resourceFile: 'tm3n3p02',
+				imageCheck: true,
+
+				headerCheck: true,
+				width: 32,
+				height: 32,
+				bitDepth: 2,
+				colorType: 3,
+				filter: 0,
+				compression: 0,
+				interlace: 0
+			});
+		});
+	});
 });
