6d64a41bcf552fdbf50a319902907700f9838fdd
Marcel Erz <erz@yahoo-inc.com>
2015-09-21 23:34:12 -0700

    Add all encoder filter tests

--- test/png/encode/filter.js
+++ test/png/encode/filter.js
@@ -18,109 +18,116 @@ describe('Filter', function () {
 			height: 32,
 
 			encodeOptions: {
+				// Should be "filter: 0"
 			}
 		});
 	});
 
-	//describe('No Filter', function () {
-  //
-	//	testGen.addEncodeTests({
-	//		resourceGroup: [],
-	//		resourceFile: 'base',
-	//		outputGroup: 'filter',
-	//		outputFile: 'no_filter',
-  //
-	//		imageCheck: true,
-	//		width: 32,
-	//		height: 32,
-  //
-	//		encodeOptions: {
-	//		}
-	//	});
-	//});
-  //
-	//describe('Sub Filter', function () {
-  //
-	//	testGen.addEncodeTests({
-	//		resourceGroup: [],
-	//		resourceFile: 'base',
-	//		outputGroup: 'filter',
-	//		outputFile: 'sub_filter',
-  //
-	//		imageCheck: true,
-	//		width: 32,
-	//		height: 32,
-  //
-	//		encodeOptions: {
-	//		}
-	//	});
-	//});
-  //
-	//describe('Up Filter', function () {
-  //
-	//	testGen.addEncodeTests({
-	//		resourceGroup: [],
-	//		resourceFile: 'base',
-	//		outputGroup: 'filter',
-	//		outputFile: 'up_filter',
-  //
-	//		imageCheck: true,
-	//		width: 32,
-	//		height: 32,
-  //
-	//		encodeOptions: {
-	//		}
-	//	});
-	//});
-  //
-	//describe('Average Filter', function () {
-  //
-	//	testGen.addEncodeTests({
-	//		resourceGroup: [],
-	//		resourceFile: 'base',
-	//		outputGroup: 'filter',
-	//		outputFile: 'average_filter',
-  //
-	//		imageCheck: true,
-	//		width: 32,
-	//		height: 32,
-  //
-	//		encodeOptions: {
-	//		}
-	//	});
-	//});
-  //
-	//describe('Paeth Filter', function () {
-  //
-	//	testGen.addEncodeTests({
-	//		resourceGroup: [],
-	//		resourceFile: 'base',
-	//		outputGroup: 'filter',
-	//		outputFile: 'paeth_filter',
-  //
-	//		imageCheck: true,
-	//		width: 32,
-	//		height: 32,
-  //
-	//		encodeOptions: {
-	//		}
-	//	});
-	//});
-  //
-	//describe('Auto Filters', function () {
-  //
-	//	testGen.addEncodeTests({
-	//		resourceGroup: [],
-	//		resourceFile: 'base',
-	//		outputGroup: 'filter',
-	//		outputFile: 'auto_filter',
-  //
-	//		imageCheck: true,
-	//		width: 32,
-	//		height: 32,
-  //
-	//		encodeOptions: {
-	//		}
-	//	});
-	//});
+	describe('No Filter', function () {
+
+		testGen.addEncodeTests({
+			resourceGroup: [],
+			resourceFile: 'base',
+			outputGroup: 'filter',
+			outputFile: 'no_filter',
+
+			imageCheck: true,
+			width: 32,
+			height: 32,
+
+			encodeOptions: {
+				filter: 0
+			}
+		});
+	});
+
+	describe('Sub Filter', function () {
+
+		testGen.addEncodeTests({
+			resourceGroup: [],
+			resourceFile: 'base',
+			outputGroup: 'filter',
+			outputFile: 'sub_filter',
+
+			imageCheck: true,
+			width: 32,
+			height: 32,
+
+			encodeOptions: {
+				filter: 1
+			}
+		});
+	});
+
+	describe('Up Filter', function () {
+
+		testGen.addEncodeTests({
+			resourceGroup: [],
+			resourceFile: 'base',
+			outputGroup: 'filter',
+			outputFile: 'up_filter',
+
+			imageCheck: true,
+			width: 32,
+			height: 32,
+
+			encodeOptions: {
+				filter: 2
+			}
+		});
+	});
+
+	describe('Average Filter', function () {
+
+		testGen.addEncodeTests({
+			resourceGroup: [],
+			resourceFile: 'base',
+			outputGroup: 'filter',
+			outputFile: 'average_filter',
+
+			imageCheck: true,
+			width: 32,
+			height: 32,
+
+			encodeOptions: {
+				filter: 3
+			}
+		});
+	});
+
+	describe('Paeth Filter', function () {
+
+		testGen.addEncodeTests({
+			resourceGroup: [],
+			resourceFile: 'base',
+			outputGroup: 'filter',
+			outputFile: 'paeth_filter',
+
+			imageCheck: true,
+			width: 32,
+			height: 32,
+
+			encodeOptions: {
+				filter: 4
+			}
+		});
+	});
+
+	describe('Auto Filters', function () {
+
+		testGen.addEncodeTests({
+			resourceGroup: [],
+			resourceFile: 'base',
+			outputGroup: 'filter',
+			outputFile: 'auto_filter',
+
+			imageCheck: true,
+			width: 32,
+			height: 32,
+
+			encodeOptions: {
+				filter: 5
+			}
+		});
+	});
 });
